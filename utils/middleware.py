132d6bf03a59cf4884d5cd60dee126f583c0a067
Admin <xfarce1@gmail.com>
2024-01-15 20:26:51 +0800

    created utils app for all the functions needed across apps created middleware.py setting default language as 'en' for admin page updated files and tested default language across pages excep for admin as 'ja'

---
+++ utils/middleware.py
@@ -0,0 +1,14 @@
+from django.utils import translation
+
+class AdminLocaleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Check if current path is for the Django admin
+        if request.path.startswith('/admin/'):
+            # Set language to English for admin
+            translation.activate('en')
+            request.LANGUAGE_CODE = 'en'
+        response = self.get_response(request)
+        return response
